d11620417e0a58eab3a2aaf3eae00e3f61e61837
nickovic <dejan.nickovic@ait.ac.at>
2024-05-10 13:21:29 +0200

    Recorded Sampler - setting filename external to the init method

--- src/verifai/samplers/recorded_optimization.py
+++ src/verifai/samplers/recorded_optimization.py
@@ -12,14 +12,16 @@ class RecordedSampler(BoxSampler):
         self.row = 0
         self.x = None
 
-        #self.x = self.df.iloc[self.row]
-
     def setFilename(self, filename):
         self.df = pd.read_csv(filename)
-        self.x = self.df.iloc[self.row]
+        self.x = tuple(list(self.df.iloc[self.row]))
+        self.row = self.row + 1
 
     def getVector(self):
-        return tuple(self.x), None
+        if self.row < len(self.df.index):
+            self.x = tuple(list(self.df.iloc[self.row]))
+            self.row = self.row + 1
+        return self.x, None
 
     def updateVector(self, vector, info, rho):
         self.rho = rho
