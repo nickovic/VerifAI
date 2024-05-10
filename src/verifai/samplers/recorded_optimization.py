455708790846244fe588e514e603f7e575eada1d
nickovic <dejan.nickovic@ait.ac.at>
2024-05-10 13:34:46 +0200

    Recorded Sampler - setting filename external to the init method

--- src/verifai/samplers/recorded_optimization.py
+++ src/verifai/samplers/recorded_optimization.py
@@ -1,11 +1,14 @@
 
 import pandas as pd
+import numpy as np
 from verifai.samplers.domain_sampler import BoxSampler
 
 class RecordedSampler(BoxSampler):
     def __init__(self, domain, params):
         super().__init__(domain)
-        from numpy import zeros, ones
+
+        self.lb = [domain.domains[idx].intervals[0][0] for idx in domain.flattenedDimension]
+        self.ub = [domain.domains[idx].intervals[0][0] for idx in domain.flattenedDimension]
 
         self.rho = None
 
@@ -14,12 +17,20 @@ class RecordedSampler(BoxSampler):
 
     def setFilename(self, filename):
         self.df = pd.read_csv(filename)
-        self.x = tuple(list(self.df.iloc[self.row]))
+        x = np.array(list(self.df.iloc[self.row]))
+        lb = np.array(self.lb)
+        ub = np.array(self.lb)
+        x = (x - lb)/(ub - lb)
+        self.x = tuple(list(x))
         self.row = self.row + 1
 
     def getVector(self):
         if self.row < len(self.df.index):
-            self.x = tuple(list(self.df.iloc[self.row]))
+            x = np.array(list(self.df.iloc[self.row]))
+            lb = np.array(self.lb)
+            ub = np.array(self.lb)
+            x = (x - lb) / (ub - lb)
+            self.x = tuple(list(x))
             self.row = self.row + 1
         return self.x, None
 
